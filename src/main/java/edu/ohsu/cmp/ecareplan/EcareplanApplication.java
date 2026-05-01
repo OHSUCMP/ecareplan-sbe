18632d7f6c8e3aaca9261ddeb2ee5da4b9f3f8d8
Matt Storer <storer@ohsu.edu>
2026-05-01 10:21:40 -0700

    renamed application classes

--- src/main/java/edu/ohsu/cmp/ecareplan/EcareplanApplication.java
+++ src/main/java/edu/ohsu/cmp/ecareplan/EcareplanApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcareplanSbeApplication {
+public class EcareplanApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EcareplanSbeApplication.class, args);
+        SpringApplication.run(EcareplanApplication.class, args);
     }
 
 }
