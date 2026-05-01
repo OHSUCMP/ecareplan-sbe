a6537fdeb874b732b139925dd2b03c96be1884d8
Matt Storer <storer@ohsu.edu>
2026-05-01 10:20:27 -0700

    added initial framework support and known dependencies

---
+++ src/test/java/edu/ohsu/cmp/ecareplan/EcareplanSbeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ohsu.cmp.ecareplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcareplanSbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
