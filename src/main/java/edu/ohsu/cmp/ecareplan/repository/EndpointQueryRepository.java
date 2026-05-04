32dfecc1e308583f35c5f0791989293ec77818e2
Matt Storer <storer@ohsu.edu>
2026-05-04 10:27:01 -0700

    added ability to get endpoint queries by endpoint ID

--- src/main/java/edu/ohsu/cmp/ecareplan/repository/EndpointQueryRepository.java
+++ src/main/java/edu/ohsu/cmp/ecareplan/repository/EndpointQueryRepository.java
@@ -2,6 +2,11 @@ package edu.ohsu.cmp.ecareplan.repository;
 
 import edu.ohsu.cmp.ecareplan.entity.EndpointQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Collection;
 
 public interface EndpointQueryRepository extends JpaRepository<EndpointQuery, Long> {
+    @Query("select e from EndpointQuery e where e.endpointId=:endpointId")
+    Collection<EndpointQuery> findByEndpointId(Long endpointId);
 }
