9c1c68f43b56365a7230e25b2d12b67941008bcd
Matt Storer <storer@ohsu.edu>
2026-05-04 10:39:10 -0700

    added AuditService

---
+++ src/main/java/edu/ohsu/cmp/ecareplan/service/AuditService.java
@@ -0,0 +1,68 @@
+package edu.ohsu.cmp.ecareplan.service;
+
+import edu.ohsu.cmp.ecareplan.entity.Audit;
+import edu.ohsu.cmp.ecareplan.entity.User;
+import edu.ohsu.cmp.ecareplan.model.AuditSeverity;
+import edu.ohsu.cmp.ecareplan.repository.AuditRepository;
+import edu.ohsu.cmp.ecareplan.workspace.UserWorkspaceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuditService {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private UserWorkspaceService userWorkspaceService;
+
+    @Autowired
+    private AuditRepository repository;
+
+    public void doAudit(String sessionId, AuditSeverity severity, String action) {
+        if (userWorkspaceService.exists(sessionId)) {
+            Long userId = userWorkspaceService.get(sessionId).getUserId();
+            doAudit(new Audit(userId, severity, action));
+
+        } else {
+            logger.warn("attempted to generate audit for nonexistent session " + sessionId + ": severity=" + severity +
+                    ", action=" + action);
+        }
+    }
+
+    public void doAudit(String sessionId, AuditSeverity severity, String action, String details) {
+        if (userWorkspaceService.exists(sessionId)) {
+            Long patId = userWorkspaceService.get(sessionId).getUserId();
+            doAudit(new Audit(patId, severity, action, details));
+
+        } else {
+            logger.warn("attempted to generate audit for nonexistent session " + sessionId + ": severity=" + severity +
+                    ", action=" + action + ", details=" + details);
+        }
+    }
+
+    public void doAudit(User user, AuditSeverity severity, String action) {
+        doAudit(user, severity, action, null);
+    }
+
+    public void doAudit(User user, AuditSeverity severity, String action, String details) {
+        doAudit(new Audit(user.getId(), severity, action, details));
+    }
+
+
+    private void doAudit(Audit audit) {
+        try {
+            if (logger.isDebugEnabled()) {
+                Audit generatedAudit = repository.save(audit);
+                logger.debug("generated " + generatedAudit);
+
+            } else {
+                repository.save(audit);
+            }
+
+        } catch (Exception e) {
+            logger.error("caught " + e.getClass().getName() + " attempting to create " + audit + " - " + e.getMessage(), e);
+        }
+    }
+}
