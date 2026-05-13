751360c52facb6a039948257bc310799a95738c3
Matt Storer <storer@ohsu.edu>
2026-05-13 16:22:48 -0700

    added findByUserIdAndEndpointId

--- src/main/java/edu/ohsu/cmp/ecareplan/repository/UserEndpointRepository.java
+++ src/main/java/edu/ohsu/cmp/ecareplan/repository/UserEndpointRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserEndpointRepository extends JpaRepository<UserEndpoint, Long> {
     @Query("select ue from UserEndpoint ue where ue.userId=:userId")
     List<UserEndpoint> findByUserId(Long userId);
+
+    @Query("select ue from UserEndpoint ue where ue.userId=:userId and ue.endpoint.id=:endpointId")
+    Optional<UserEndpoint> findByUserIdAndEndpointId(Long userId, Long endpointId);
 }
