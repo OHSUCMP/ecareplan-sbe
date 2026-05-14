adc0fac16f5585bd2aaaaf7ea964181e99426d9d
Matt Storer <storer@ohsu.edu>
2026-05-14 14:59:04 -0700

    modified to specify String representations of enum types

--- src/main/java/edu/ohsu/cmp/ecareplan/model/EndpointProviderType.java
+++ src/main/java/edu/ohsu/cmp/ecareplan/model/EndpointProviderType.java
@@ -1,7 +1,31 @@
 package edu.ohsu.cmp.ecareplan.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum EndpointProviderType {
-    GENERIC,
-    EPIC,
-    ORACLE
+    GENERIC(null),
+    EPIC("epic"),
+    ORACLE("oracle");
+
+    private static final Map<String, EndpointProviderType> TAG_MAP = new HashMap<>();
+    static {
+        for (EndpointProviderType ept : values()) {
+            TAG_MAP.put(ept.tag, ept);
+        }
+    }
+
+    public static EndpointProviderType fromTag(String tag) {
+        return TAG_MAP.get(tag);
+    }
+
+    private final String tag;
+
+    EndpointProviderType(String tag) {
+        this.tag = tag;
+    }
+
+    public String getTag() {
+        return tag;
+    }
 }
