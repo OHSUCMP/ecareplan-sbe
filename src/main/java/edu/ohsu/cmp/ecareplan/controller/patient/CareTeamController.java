9296ae3503a1d838b6b404ee2ea07ba8ec9c2be0
Matt Storer <storer@ohsu.edu>
2026-06-02 13:15:39 -0700

    added empty controller placeholders for each of the pages in the patient application

---
+++ src/main/java/edu/ohsu/cmp/ecareplan/controller/patient/CareTeamController.java
@@ -0,0 +1,9 @@
+package edu.ohsu.cmp.ecareplan.controller.patient;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/patient/care-team")
+public class CareTeamController {
+}
