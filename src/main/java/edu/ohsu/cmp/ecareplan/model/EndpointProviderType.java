7db5c7c461c71054b193f5654b995b3ecc331447
Matt Storer <storer@ohsu.edu>
2026-06-11 11:21:02 -0700

    simplified to remove tags (they don't deserialize well from database fields)

--- src/main/java/edu/ohsu/cmp/ecareplan/model/EndpointProviderType.java
+++ src/main/java/edu/ohsu/cmp/ecareplan/model/EndpointProviderType.java
@@ -1,31 +1,7 @@
 package edu.ohsu.cmp.ecareplan.model;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public enum EndpointProviderType {
-    GENERIC("generic"),
-    EPIC("epic"),
-    ORACLE("oracle");
-
-    private static final Map<String, EndpointProviderType> TAG_MAP = new HashMap<>();
-    static {
-        for (EndpointProviderType ept : values()) {
-            TAG_MAP.put(ept.tag, ept);
-        }
-    }
-
-    public static EndpointProviderType fromTag(String tag) {
-        return TAG_MAP.get(tag);
-    }
-
-    private final String tag;
-
-    EndpointProviderType(String tag) {
-        this.tag = tag;
-    }
-
-    public String getTag() {
-        return tag;
-    }
+    GENERIC,
+    EPIC,
+    ORACLE;
 }
