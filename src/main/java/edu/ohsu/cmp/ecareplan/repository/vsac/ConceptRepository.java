a0541ad4d8d977eb8e59c5a2e12d1e85b9b43314
Matt Storer <storer@ohsu.edu>
2026-06-18 10:06:00 -0700

    modified function names to use JPA query methods, and added a version of the findConcept function that finds the most recent version of a concept if no version is specified

--- src/main/java/edu/ohsu/cmp/ecareplan/repository/vsac/ConceptRepository.java
+++ src/main/java/edu/ohsu/cmp/ecareplan/repository/vsac/ConceptRepository.java
@@ -2,10 +2,8 @@ package edu.ohsu.cmp.ecareplan.repository.vsac;
 
 import edu.ohsu.cmp.ecareplan.entity.vsac.Concept;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ConceptRepository extends JpaRepository<Concept, Long> {
-
-    @Query("select c from Concept c where c.code=:code and c.codeSystem=:codeSystem and c.codeSystemVersion=:version")
-    Concept findConcept(String code, String codeSystem, String version);
+    Concept findByCodeAndCodeSystemAndCodeSystemVersion(String code, String codeSystem, String version);
+    Concept findFirstByCodeAndCodeSystemOrderByCodeSystemVersionDesc(String code, String codeSystem);
 }
