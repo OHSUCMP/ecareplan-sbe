d7d21f7518448385c3b2032ceae8e6c66611044a
Matt Storer <storer@ohsu.edu>
2026-06-25 13:55:35 -0700

    added initial populated Immunizations model and UI

--- src/main/java/edu/ohsu/cmp/ecareplan/model/dataset/ImmunizationModel.java
+++ src/main/java/edu/ohsu/cmp/ecareplan/model/dataset/ImmunizationModel.java
@@ -2,9 +2,33 @@ package edu.ohsu.cmp.ecareplan.model.dataset;
 
 import org.hl7.fhir.r4.model.Immunization;
 
+import java.util.Date;
+import java.util.List;
+
 public class ImmunizationModel extends BaseDataSetModel<Immunization> {
+    private String description;
+    private Date effectiveDate;
+    private String location;
+    private List<String> notes;
+
     public ImmunizationModel(Immunization immunization) {
         super(immunization);
+
+        if (immunization.hasVaccineCode()) {
+            description = getConceptNameFromCodeableConcept(immunization.getVaccineCode());
+        }
+
+        if (immunization.hasOccurrenceDateTimeType()) {
+            effectiveDate = immunization.getOccurrenceDateTimeType().getValue();
+        }
+
+        if (immunization.hasLocation() && immunization.getLocation().hasDisplay()) {
+            location = immunization.getLocation().getDisplay();
+        }
+
+        if (immunization.hasNote()) {
+            notes = buildNotes(immunization.getNote());
+        }
     }
 
     @Override
