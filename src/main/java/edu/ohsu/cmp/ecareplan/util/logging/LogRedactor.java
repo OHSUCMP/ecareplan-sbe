1f99016d747baabefd431d2833596aaa6642bd63
Matt Storer <storer@ohsu.edu>
2026-07-22 12:35:16 -0700

    improved patient ID redactions for #5

--- src/main/java/edu/ohsu/cmp/ecareplan/util/logging/LogRedactor.java
+++ src/main/java/edu/ohsu/cmp/ecareplan/util/logging/LogRedactor.java
@@ -1,13 +1,16 @@
 package edu.ohsu.cmp.ecareplan.util.logging;
 
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LogRedactor {
     private static final String REDACTION = "[REDACTED]";
 
     private static final List<Pattern> REDACTION_PATTERNS = List.of(
-            Pattern.compile("(Patient/[^&\\s]+)")
+            Pattern.compile("Patient/([^&\"\\s]+)"),
+            Pattern.compile("patient=([^&\"\\s]+)"),
+            Pattern.compile("subject=([^&\"\\s]+)")
     );
 
     private LogRedactor() {
@@ -21,9 +24,29 @@ public class LogRedactor {
         String redacted = value;
 
         for (Pattern pattern : REDACTION_PATTERNS) {
-            redacted = pattern.matcher(redacted).replaceAll(REDACTION);
+            redacted = redactFirstGroup(redacted, pattern);
         }
 
         return redacted;
+    }
+
+    private static String redactFirstGroup(String value, Pattern pattern) {
+        Matcher matcher = pattern.matcher(value);
+        StringBuilder redacted = new StringBuilder();
+
+        while (matcher.find()) {
+            int groupStartInMatch = matcher.start(1) - matcher.start();
+            int groupEndInMatch = matcher.end(1) - matcher.start();
+
+            String match = matcher.group();
+            String redactedMatch = match.substring(0, groupStartInMatch) +
+                    REDACTION +
+                    match.substring(groupEndInMatch);
+
+            matcher.appendReplacement(redacted, Matcher.quoteReplacement(redactedMatch));
+        }
+
+        matcher.appendTail(redacted);
+        return redacted.toString();
     }
 }
