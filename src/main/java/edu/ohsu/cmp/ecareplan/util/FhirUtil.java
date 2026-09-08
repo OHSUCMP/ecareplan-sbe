e476e0a44e612256fc33a66d01d81860fea150c9
Matt Storer <storer@ohsu.edu>
2026-09-08 12:30:00 -0700

    reverted some null handling I just added to enforce fail-fast operation

--- src/main/java/edu/ohsu/cmp/ecareplan/util/FhirUtil.java
+++ src/main/java/edu/ohsu/cmp/ecareplan/util/FhirUtil.java
@@ -496,15 +496,11 @@ public class FhirUtil {
     }
 
     public static String toJson(IBaseResource r) {
-        return r != null ?
-                FHIR_R4_CONTEXT_AUXILIARY.newJsonParser().encodeResourceToString(r) :
-                "";
+        return FHIR_R4_CONTEXT_AUXILIARY.newJsonParser().encodeResourceToString(r);
     }
 
     public static <R extends IBaseResource> R fromJson(Class<R> clazz, String json) {
-        return clazz == null || StringUtils.isBlank(json) ?
-                null :
-                FHIR_R4_CONTEXT_AUXILIARY.newJsonParser().parseResource(clazz, json);
+        return FHIR_R4_CONTEXT_AUXILIARY.newJsonParser().parseResource(clazz, json);
     }
 
     private static final Pattern ID_PATTERN = Pattern.compile("\"id\":\\s+\"([^\"]+)\"");
